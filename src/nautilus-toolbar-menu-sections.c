d71963c04d684aaaea9a21e4ea3a32c71fd03400
Neil Herald <neil.herald@gmail.com>
2016-06-19 17:35:49 +0100

    view: allow view to have more control over the toolbar menu
    Currently we have this menu structure:
    
    ------------------------------
    1. New Folder/New Tab/Bookmark
    ------------------------------
    2. Zoom controls
    ------------------------------
    3. Undo/Redo
    ------------------------------
    4. Sort options
    ------------------------------
    5. Other view related controls
    ------------------------------
    
    The view creates 2-5, contained in a single GtkWidget - which is then
    passed to the toolbar via the enclosing window slot. The problem is that
    3 shouldn't be created or managed by the view as the controls in that
    section of the menu are not related to the view. We'd like to move this
    responsibility back to the toolbar, but that would mean the view must
    pass multiple menu sections back to the toolbar (as 3 is in the middle
    of the other view controls).
    
    This change allows the view to pass multiple sections back to the
    toolbar, using the new NautilusToolbarMenuSections structure. The files
    view now passes 2 as a separate section to 3-5 (3 will be moved out of
    the view in a future commit).
    
    https://bugzilla.gnome.org/show_bug.cgi?id=764632

---
+++ src/nautilus-toolbar-menu-sections.c
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2016 Neil Herald <neil.herald@gmail.com>
+ *
+ * Nautilus is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * Nautilus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <gtk/gtk.h>
+
+#include "nautilus-toolbar-menu-sections.h"
+
+
+NautilusToolbarMenuSections *
+nautilus_toolbar_menu_sections_new (void)
+{
+        NautilusToolbarMenuSections *sections;
+
+        sections = g_new0 (NautilusToolbarMenuSections, 1);
+
+        return sections;
+}
